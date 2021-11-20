754fef07ff2a9d58d218ae48612bdea315cb94ba
Lilly <ddzwonczyk@live.com>
2021-11-20 13:42:35 -0500

    another change

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app_route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index_html')
 
